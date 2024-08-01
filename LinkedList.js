e14ca9ddb0f52fb93c345d45dfae93656135c734
Joe Chek <jmchek@gmail.com>
2024-08-01 10:50:44 -0700

    initialize the base files for this project

---
+++ LinkedList.js
@@ -0,0 +1,4 @@
+export default function LinkedList() {
+
+    return { value, nextNode };
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+import LinkedList from "./LinkedList.js";
+import Node from "./Node.js";
+
+
+
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
