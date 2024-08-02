c6edfa5e747afdb67564b98f6fb47df9cc4b0db4
Joe Chek <jmchek@gmail.com>
2024-08-02 14:35:17 -0700

    finalize methods for LinkedList()

--- LinkedList.js
+++ LinkedList.js
@@ -1,16 +1,18 @@
 import Node from "./Node.js";
 
 const LinkedList = () => {
-    let head = null;
+    let HEAD = null;
+    let length = 0;
 
     const append = (value) => {
         const newNode = Node(value);
         
-        if(head === null){
-            return (head = newNode);
+        if(HEAD === null){
+            length++;
+            return (HEAD = newNode);
         } 
-        
-        let tmp = head;
+        length++;
+        let tmp = HEAD;
 
         while(tmp.next !== null){
             tmp = tmp.next;
@@ -21,25 +23,166 @@ const LinkedList = () => {
         
     };
 
-    // const prepend = (value) => {
-    //     let tmp = head;
-    //     head = Node(value);
-    //     head.next = tmp;
-    // };
+    const prepend = (value) => {
+        const newNode = Node(value);
 
-    // const size = () => {
-    //     let count = 0;
-    //     let tmp = head;
+        if(HEAD === null){
+            length++;
+            return (HEAD = newNode);
+        }
+        length++;
+        let tmp = HEAD;
+        HEAD = Node(value);
+        HEAD.next = tmp;
+    };
 
-    //     while(tmp){
-    //         count++;
-    //         tmp = tmp.next;
-    //     }
+    const size = () => {
+        if (!HEAD) return null;
+        else return length;
+    };
 
-    //     return count;
-    // }
+    const head = () => {
+        if (!HEAD) return null;
+        return HEAD.value;
+    };
 
-    return { append };
+    const tail = () => {
+        if (!HEAD) return null;
+
+        let tmp = HEAD;
+
+        while(tmp){
+            tmp = tmp.next;
+            if (tmp.next == null){
+                return tmp.value;
+            }
+        }
+    }
+
+    const at = (index) => {
+        let count = 0;
+        let current = HEAD;
+
+        while (count != index){
+            count++;
+            current = current.next;
+        }
+
+        return current.value;
+    }
+
+    const pop = () => {
+        let current = HEAD;
+
+        while (current.next.next !== null){
+            current = current.next;
+            if (current.next.next === null){
+                current.next = null;
+                return;
+            }
+        }
+    }
+
+    const contains = (value) => {
+        let current = HEAD;
+
+        while(current){
+            if (current.value == value){
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    const find = (value) => {
+        let count = 0;
+        let current = HEAD;
+
+        while (current){
+            if(current.value == value){
+                return count;
+            }
+
+            count++;
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    const toString = () => {
+        let current = HEAD;
+        let stringedList = "";
+
+        while (current){
+            stringedList += "( " + current.value + " )";
+            if(current.next !== null){
+                stringedList += " -> ";
+            }
+            current = current.next;
+        }
+
+        stringedList += " -> " + null;
+
+        return stringedList;
+    }
+
+    const insertAt = (value, index) => {
+        let count = 0;
+        let current = HEAD;
+        const newNode = Node(value);
+        length++;
+
+        if(HEAD === null){
+            return (HEAD = newNode);
+        } else if (index >= length || index < 0){
+            return console.log("Out of range.")
+        } else if(index == 0){
+            length++;
+            let tmp = HEAD;
+            HEAD = Node(value);
+            HEAD.next = tmp;
+        }
+
+        while (count < (index - 1)){
+            count++;
+            current = current.next;
+        }
+
+        let tmp = current;
+        tmp = tmp.next;
+        current.next = newNode;
+        current = current.next;
+        current.next = tmp;
+    }
+
+    const removeAt = (index) => {
+        let count = 0;
+        let current = HEAD;
+
+        if(HEAD === null || index >= length || index < 0){
+            return console.log("No values to remove.");
+        }
+
+        length--;
+
+        while (count < (index - 1)){
+            count++;
+            current = current.next;
+            
+        }
+        if(current.next.next !== null && index != 0){
+            current.next = current.next.next;
+        } else if (index == 0){
+            HEAD = HEAD.next;
+        } else {
+            current.next = null;
+        }
+    }
+
+    return { append, prepend, size, head, tail, at, pop, contains, find, toString, insertAt, removeAt };
 }
 
 export default LinkedList;
--- main.js
+++ main.js
@@ -2,26 +2,14 @@ import LinkedList from "./LinkedList.js";
 import Node from "./Node.js";
 
 
-
-// let node1 = Node(2);
-// let node2 = Node(5);
-// node1.next = node2;
-
+// example uses class syntax - adjust as necessary
 const list = LinkedList();
 
-list.append(7);
-list.append(2);
-list.append(5);
-console.log(list.head);
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
-// console.log(list.head.value);
-
-// example uses class syntax - adjust as necessary
-// const list = new LinkedList();
-
-// list.append("dog");
-// list.append("cat");
-// list.append("parrot");
-// list.append("hamster");
-// list.append("snake");
-// list.append("turtle");
+console.log(list.toString());
